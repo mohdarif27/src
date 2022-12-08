18a5c91c3e9ea4a2e111ae5086b2c88e599fcd40
Arif <115528818+mohdarif27@users.noreply.github.com>
2022-12-09 00:21:24 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
-	SystemSystem.out.println("Hello world !!!!");    
+	SystemSystem.out.println("my first CI example");    
     }  
 }  
