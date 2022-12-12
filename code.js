b263f0478f1f782f7b5f1108195b3c978f76e946
Arif <115528818+mohdarif27@users.noreply.github.com>
2022-12-12 20:01:07 +0530

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,5 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
 	SystemSystem.out.println("my first CI example");    
-	    SystemSystem.out.println("Building through my second pipeline!!!");  
-    }  
+	        }  
 }  
